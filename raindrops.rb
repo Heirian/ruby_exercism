80e952156cd0c09324aac4041d4d9c0ac01ba0dc
Bruno Teófilo <shinkcc@gmail.com>
2017-07-13 12:15:02 -0300

    Update raindrops.rb

--- raindrops.rb
+++ raindrops.rb
@@ -1,10 +1,11 @@
+# Raindrops custom class
 class Raindrops
   def self.convert(number_drop)
     rain_sound = ''
 
-    rain_sound << 'Pling' if number_drop % 3 == 0
-    rain_sound << 'Plang' if number_drop % 5 == 0
-    rain_sound << 'Plong' if number_drop % 7 == 0
+    rain_sound << 'Pling' if (number_drop % 3).zero?
+    rain_sound << 'Plang' if (number_drop % 5).zero?
+    rain_sound << 'Plong' if (number_drop % 7).zero?
 
     return number_drop.to_s if rain_sound.empty?
 
