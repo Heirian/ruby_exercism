8698629dabe1ebf4b332fd77aa8a56de9b64bd12
Bruno Teófilo <shinkcc@gmail.com>
2017-07-17 20:29:20 -0300

    Update hamming.rb

--- hamming.rb
+++ hamming.rb
@@ -1,35 +1,17 @@
+# the distance of a DNA to your replica
 class Hamming
-  def self.compute(initial, replica)
-
-    raise ArgumentError if initial.length != replica.length
-
+  def self.compute(original, mutation)
+    warning = 'The strands must have the same length'
+    raise ArgumentError, warning if original.length != mutation.length
     hamming_distance = 0
-    dna_index = 0
-
-    while dna_index < initial.length
-      hamming_distance += 1 if initial[dna_index] != replica[dna_index]
-      dna_index += 1
+    paired_strands = original.chars.zip(mutation.chars)
+    paired_strands.count do |(initial, replica)|
+      hamming_distance += 1 unless initial == replica
     end
     hamming_distance
   end
 end
 
 module BookKeeping
-   VERSION = 3
-end
-
-#OR
-
-class Hamming
-  def self.compute(initial, replica)
-
-    raise ArgumentError if initial.length != replica.length
-
-    hamming_distance = 0
-    for nucleotide_index in 0...initial.length
-      hamming_distance += 1 if initial[nucleotide_index] != replica[nucleotide_index]
-    end
-
-    hamming_distance
-  end
+  VERSION = 3
 end
