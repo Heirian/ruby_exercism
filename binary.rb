a20454a9379056a5b1f9e6cda10e356d4cda68a2
Bruno Teófilo <shinkcc@gmail.com>
2017-07-19 21:00:14 -0300

    Update binary.rb

--- binary.rb
+++ binary.rb
@@ -2,13 +2,11 @@
 class Binary
   @bin_num = %w[0 1]
   def self.to_decimal(input)
-    sequence = input.reverse
     sum = 0
-    sequence.each_char.with_index do |value, index|
+    input.reverse.each_char.with_index do |value, index|
       raise ArgumentError unless @bin_num.include?(value)
       sum += value.to_i * (2**index)
     end
-    sum
   end
 end
 
