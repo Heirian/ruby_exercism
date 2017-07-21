027c353741b775001e6446a8257f529170917450
Bruno Teófilo <shinkcc@gmail.com>
2017-07-21 00:51:13 -0300

    Update romans_numerals.rb

--- romans_numerals.rb
+++ romans_numerals.rb
@@ -1,73 +1,36 @@
-class Fixnum
+# Custom roman method to Fixnum
+module RomanNumerals
+  TO_ROMANS = {
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+  }
+
   def to_roman
-    unit = {
-      "0" => "",
-      "1" => "I",
-      "2" => "II",
-      "3" => "III",
-      "4" => "IV",
-      "5" => "V",
-      "6" => "VI",
-      "7" => "VII",
-      "8" => "VIII",
-      "9" => "IX"
-    }
-
-    tens = {
-      "0" => "",
-      "1" => "X",
-      "2" => "XX",
-      "3" => "XXX",
-      "4" => "XL",
-      "5" => "L",
-      "6" => "LX",
-      "7" => "LXX",
-      "8" => "LXXX",
-      "9" => "XC"
-    }
-
-    hundreds = {
-      "0" => "",
-      "1" => "C",
-      "2" => "CC",
-      "3" => "CCC",
-      "4" => "CD",
-      "5" => "D",
-      "6" => "DC",
-      "7" => "DCC",
-      "8" => "DCCC",
-      "9" => "CM"
-    }
-
-    thousands = {
-      "1" => "M",
-      "2" => "MM",
-      "3" => "MMM",
-    }
-
-    roman_number = ""
-
-    number = self.to_s
-
-    if self < 10
-      roman_number << unit[number[0]]
-    elsif self < 100
-      roman_number << tens[number[0]]
-      roman_number << unit[number[1]]
-    elsif self < 1000
-      roman_number << hundreds[number[0]]
-      roman_number << tens[number[1]]
-      roman_number << unit[number[2]]
-    elsif self < 4000
-      roman_number << thousands[number[0]]
-      roman_number << hundreds[number[1]]
-      roman_number << tens[number[2]]
-      roman_number << unit[number[3]]
-    end
-
-
-    roman_number
+      translate(self).join
   end
+
+  private
+    def translate(num)
+      TO_ROMANS.each_with_object([]) do |(key, value), rom |
+          rom << value * (num / key)
+          num %= key
+      end
+    end
+end
+
+class Fixnum
+  include RomanNumerals
 end
 
 module BookKeeping
