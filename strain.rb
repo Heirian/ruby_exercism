1aff589d2fe57c2d5efdd3b7443dfcae882e87a4
Bruno Teófilo <shinkcc@gmail.com>
2017-07-20 21:47:42 -0300

    Update strain.rb

--- strain.rb
+++ strain.rb
@@ -14,3 +14,14 @@ class Array
     output
   end
 end
+
+# array class custom methods
+class Array
+  def keep
+    each_with_object([]) { |value, output| output << value if yield(value) }
+  end
+
+  def discard
+    each_with_object([]) { |value, output| output << value unless yield(value) }
+  end
+end
