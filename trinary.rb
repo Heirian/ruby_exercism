c1d6d8a1b0fef41721484406c732c0d78b4f4bc2
Bruno Teófilo <shinkcc@gmail.com>
2017-07-22 10:42:38 -0300

    Update trinary.rb

--- trinary.rb
+++ trinary.rb
@@ -1,22 +1,22 @@
 # trinary custom class
 class Trinary
+  def to_decimal
+    translation
+  end
+
+  private
+
   def initialize(input)
     @input = input.reverse
     @num_list = %w[0 1 2]
     @base = @num_list.size
   end
 
-  def to_decimal
-    translation.reduce(:+)
-  end
-
-  private
-
   def translation
     @input.chars.each_with_index.with_object([]) do |(value, index), nova|
-      return [0] unless @num_list.include?(value)
+      return 0 unless @num_list.include?(value)
       nova << value.to_i * (@base**index)
-    end
+    end.reduce(:+)
   end
 end
 
