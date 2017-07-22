eabb1b7ed1d0f2948dfb408dd875fe3f37dbed4e
Bruno Teófilo <shinkcc@gmail.com>
2017-07-22 10:41:50 -0300

    Update romans_numerals.rb

--- romans_numerals.rb
+++ romans_numerals.rb
@@ -1,34 +1,36 @@
-# Custom roman method to Fixnum
+# Custom module roman numerals
 module RomanNumerals
   TO_ROMANS = {
-      1000 => "M",
-      900 => "CM",
-      500 => "D",
-      400 => "CD",
-      100 => "C",
-      90 => "XC",
-      50 => "L",
-      40 => "XL",
-      10 => "X",
-      9 => "IX",
-      5 => "V",
-      4 => "IV",
-      1 => "I"
-  }
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }.freeze
 
   def to_roman
-      translate(self).join
+    translate(self)
   end
 
   private
-    def translate(num)
-      TO_ROMANS.each_with_object([]) do |(key, value), rom |
-          rom << value * (num / key)
-          num %= key
-      end
-    end
+
+  def translate(num)
+    TO_ROMANS.each_with_object([]) do |(key, value), rom|
+      rom << value * (num / key)
+      num %= key
+    end.join
+  end
 end
 
+# Custom roman method to Fixnum
 class Fixnum
   include RomanNumerals
 end
